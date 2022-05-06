77f19803f2df79a839a39d58a61ec0e39cc241fe
Mohamed Haroon <37942918+MohamedHaroon@users.noreply.github.com>
2022-05-06 23:50:28 +0200

    Update server.py

--- final_project/server.py
+++ final_project/server.py
@@ -7,13 +7,19 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
+    translation = language_translator.translate(
+    text=textToTranslate,
+    model_id='en-fr').get_result()
+    french_text = translation['translations'][0]['translation']
     return "Translated text to French"
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
+    translation = language_translator.translate(
+    text=textToTranslate,
+    model_id='fr-en').get_result()
+    english_text = translation['translations'][0]['translation']
     return "Translated text to English"
 
 @app.route("/")
